f5d8dade166f677b2af8984e4ddb6a2a24a4686b
mouasit <mustaphaouassit60@gmail.com>
2023-04-08 19:01:22 +0000

    add: product section

--- src/Components/BoxImg.js
+++ src/Components/BoxImg.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import productOne from "../assets/images/image-product-1.jpg";
+import productTwo from "../assets/images/image-product-2.jpg";
+import { IconLeftArrow, IconRightArrow } from "./Icons";
 export default function BoxImg() {
   return (
     <section>
-      
+      <div className="relative flex items-center">
+        <img src={productOne} alt="product thumbnail" className="w-full" />
+        <button
+          aria-label="previous arrow"
+          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 ml-5 lg:hidden"
+        >
+          <IconLeftArrow />
+        </button>
+        <button
+          aria-label="next arrow"
+          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 mr-5 right-0 lg:hidden"
+        >
+          <IconRightArrow />
+        </button>
+      </div>
+      <div>
+        <button aria-label="productOne"></button>
+      </div>
     </section>
-  )
+  );
 }
--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -23,3 +23,31 @@ export function IconCart() {
     </svg>
   );
 }
+
+export function IconLeftArrow() {
+  return (
+    <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11 1 3 9l8 8"
+        stroke="#1D2026"
+        stroke-width="3"
+        fill="none"
+        fill-rule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function IconRightArrow() {
+  return (
+    <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m2 1 8 8-8 8"
+        stroke="#1D2026"
+        stroke-width="3"
+        fill="none"
+        fill-rule="evenodd"
+      />
+    </svg>
+  );
+}
