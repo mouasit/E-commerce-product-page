0520367ccb5db289c5b998c6c513ee43159d489b
mouasit <mustaphaouassit60@gmail.com>
2023-04-09 17:18:46 +0000

    fix button

--- src/Components/BoxImg.js
+++ src/Components/BoxImg.js
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import { IconLeftArrow, IconRightArrow } from "./Icons";
-import { thumbnails,imgProducts } from "../data";
+import { thumbnails, imgProducts } from "../data";
 import ListImg from "./ListImg";
 export default function BoxImg() {
   const [state, setState] = useState(0);
   let count = 0;
   return (
     <section className="lg:flex lg:flex-col lg:gap-8">
-      <div className="relative flex items-center">
-        <img src={imgProducts[state]} alt="product" className="w-full lg:rounded-xl" />
+      <div className="relative flex items-center lg:hidden">
+        <img
+          src={imgProducts[state]}
+          alt="product"
+          className="w-full"
+        />
         <button
           aria-label="previous arrow"
-          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 ml-5 lg:hidden"
+          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 ml-5"
         >
           <IconLeftArrow />
         </button>
         <button
           aria-label="next arrow"
-          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 mr-5 right-0 lg:hidden"
+          className="absolute bg-white flex justify-center items-center rounded-full w-9 h-9 mr-5 right-0"
         >
           <IconRightArrow />
         </button>
       </div>
+      <button className="hidden lg:block rounded-2xl overflow-hidden">
+        <img src={imgProducts[state]} alt="product" className="w-full" />
+      </button>
       <div className="hidden lg:flex items-center gap-8">
         {thumbnails.map((e, index) => {
           return (
